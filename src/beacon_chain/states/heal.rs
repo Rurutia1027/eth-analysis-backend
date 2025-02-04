1828e75c6ae808addd3cd8bee932143407dd506b
Rurutia1027 <rurutia1027@gmail.com>
2025-02-04 17:59:06 +0800

    add beacon_statees table basic operaiton to it mod.rs

--- src/beacon_chain/states/heal.rs
+++ src/beacon_chain/states/heal.rs
@@ -1,4 +1,4 @@
-use crate::{beacon_chain::node::BeaconNode, db};
+use crate::{beacon_chain::node::BeaconNode, db, kv_store};
 use crate::{
     beacon_chain::{self, node::BeaconNodeHttp, sync, Slot},
     job::job_progress::JobProgress,
@@ -13,4 +13,17 @@ const FIRST_SHARED_ETH_SUPPLY_SLOT: Slot = Slot(0);
 
 const HEAL_BEACON_STATES_KEY: &str = "heal-beacon-states";
 
-pub async fn heal_beacon_states() {}
+pub async fn heal_beacon_states() {
+    info!("healing reorged states");
+
+    let db_pool = db::get_db_pool("heal-beacon-states", 1).await;
+    let kv_store = kv_store::KVStorePostgres::new(db_pool.clone());
+    let job_tracer: JobProgress<'_, Slot> =
+        JobProgress::new(HEAL_BEACON_STATES_KEY, &kv_store);
+    let beacon_node = BeaconNodeHttp::new();
+    let last_slot = beacon_chain::states::get_last_state(&db_pool)
+        .await
+        .expect("a beacon state should be stored before trying to heal any")
+        .slot
+        .0;
+}
--- src/beacon_chain/states/mod.rs
+++ src/beacon_chain/states/mod.rs
@@ -3,3 +3,101 @@ pub mod heal;
 use super::Slot;
 pub use heal::heal_beacon_states;
 use sqlx::PgExecutor;
+
+#[derive(PartialEq, Debug)]
+pub struct BeaconState {
+    pub slot: Slot,
+    pub state_root: String,
+}
+
+// get the freshest(latest) record from table beacon_states
+pub async fn get_last_state(
+    executor: impl PgExecutor<'_>,
+) -> Option<BeaconState> {
+    sqlx::query_as!(
+        BeaconState,
+        r#"
+        SELECT
+            beacon_states.state_root,
+            beacon_states.slot AS "slot: Slot"
+        FROM beacon_states
+        ORDER BY slot DESC
+        LIMIT 1
+        "#,
+    )
+    .fetch_optional(executor)
+    .await
+    .unwrap()
+}
+
+// save beacon state record to table beacon_states
+pub async fn store_state(
+    executor: impl PgExecutor<'_>,
+    state_root: &str,
+    slot: Slot,
+) {
+    sqlx::query!(
+        "
+        INSERT INTO
+            beacon_states
+            (state_root, slot)
+        VALUES
+            ($1, $2)
+        ",
+        state_root,
+        slot.0,
+    )
+    .execute(executor)
+    .await
+    .unwrap();
+}
+
+pub async fn get_state_root_by_slot(
+    executor: impl PgExecutor<'_>,
+    slot: Slot,
+) -> Option<String> {
+    sqlx::query!(
+        "
+        SELECT
+            state_root
+        FROM
+            beacon_states
+        WHERE
+            slot = $1
+        ",
+        slot.0
+    )
+    .fetch_optional(executor)
+    .await
+    .unwrap()
+    .map(|row| row.state_root)
+}
+
+pub async fn delete_states(
+    executor: impl PgExecutor<'_>,
+    greater_than_or_equal: Slot,
+) {
+    sqlx::query!(
+        "
+        DELETE FROM beacon_states
+        WHERE beacon_states.slot >= $1
+        ",
+        greater_than_or_equal.0
+    )
+    .execute(executor)
+    .await
+    .unwrap();
+}
+
+pub async fn delete_state(executor: impl PgExecutor<'_>, slot: Slot) {
+    sqlx::query!(
+        "
+        DELETE FROM beacon_states
+        WHERE slot = $1
+        ",
+        slot.0
+    )
+    .execute(executor)
+    .await
+    .unwrap();
+}
