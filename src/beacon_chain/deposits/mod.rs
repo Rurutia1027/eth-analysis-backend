da7c0e3f17912f2330dd6ac80073703f209c7eee
Rurutia1027 <rurutia1027@gmail.com>
2025-02-05 17:35:15 +0800

    add records to both beacon_states and beacon_blocks table and calculate the given block deposit sum value

--- src/beacon_chain/deposits/mod.rs
+++ src/beacon_chain/deposits/mod.rs
@@ -38,3 +38,85 @@ pub async fn get_deposit_sum_aggregated(
     // block's parent deposit sum value + current block's all deposit amount value together
     parent_deposit_sum_aggregated + get_deposit_sum_from_block(block)
 }
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct BeaconDepositsSum {
+    pub deposits_sum: GweiNewtype,
+    pub slot: Slot,
+}
+
+pub async fn get_deposits_sum_by_state_root(
+    executor: impl PgExecutor<'_>,
+    state_root: &str,
+) -> Result<GweiNewtype> {
+    let deposit_sum_aggregated = sqlx::query(
+        "
+                SELECT
+                    deposit_sum_aggregated
+                FROM
+                    beacon_blocks
+                WHERE
+                    state_root = $1
+            ",
+    )
+    .bind(state_root)
+    .map(|row: PgRow| row.get::<i64, _>("deposit_sum_aggregated").into())
+    .fetch_one(executor)
+    .await?;
+
+    Ok(deposit_sum_aggregated)
+}
+
+#[cfg(test)]
+mod tests {
+    use sqlx::Acquire;
+
+    use crate::{
+        beacon_chain::{
+            blocks::store_block, states::store_state, BeaconBlockBuilder,
+            BeaconHeaderSignedEnvelopeBuilder,
+        },
+        db::db,
+    };
+
+    use super::*;
+
+    #[tokio::test]
+    async fn get_deposits_sum_by_state_root_test() {
+        let mut connection = db::tests::get_test_db_connection().await;
+        let mut transaction = connection.begin().await.unwrap();
+        let test_id = "get_deposits_sum_by_state_root";
+        let test_header =
+            BeaconHeaderSignedEnvelopeBuilder::new(test_id).build();
+        let test_block = Into::<BeaconBlockBuilder>::into(&test_header).build();
+
+        // first save record to beacon_states
+        store_state(
+            &mut *transaction,
+            &test_header.state_root(),
+            test_header.slot(),
+        )
+        .await;
+
+        // then save record to beacon_blocks
+        store_block(
+            &mut *transaction,
+            &test_block,
+            &GweiNewtype(0),
+            &GweiNewtype(1),
+            &GweiNewtype(0),
+            &GweiNewtype(1),
+            &test_header,
+        )
+        .await;
+
+        // sum = current block's all deposit#amount + current block's parent's block_states#deposit_aggreated_sum field value
+        let deposits_sum = get_deposits_sum_by_state_root(
+            &mut *transaction,
+            &test_header.state_root(),
+        ).await.unwrap();
+
+        assert_eq!(GweiNewtype(1), deposits_sum)
+    }
+}
