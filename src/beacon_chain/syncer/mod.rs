baa847e31eb2b7c9259db87313b948c32fe1249e
Rurutia1027 <rurutia1027@gmail.com>
2025-02-07 20:31:02 +0800

    add codes for find latest slot value that belonts to both on-chain and off-chain  state_root scope

--- src/beacon_chain/syncer/mod.rs
+++ src/beacon_chain/syncer/mod.rs
@@ -491,3 +491,60 @@ async fn estimate_slots_remaining(
     debug!("#estimate_slots_remaining {}", lag);
     return lag;
 }
+
+// search db's beacon_states table
+// first query state_root value from beacon_states via given starting_candidate value
+// second query beacon endpoint to fetch the given starting_candidate's state_root value
+// if beacon on chain state value match with the local given slot's state_root value , then the given slot value is the `last_matching_slot` value return
+// otherwise, decrease the value of the given slot(starting_candidate) as candidate_slot value and take this `candidate_slot` value
+// query -> from local db's beacon-states table's state_root value off-chain
+// query -> from remote beacon url endpoint's state_root value  on-chain
+// continue compare
+async fn find_last_matching_slot(
+    db_pool: &PgPool,
+    beacon_node: &BeaconNodeHttp,
+    starting_candidate: Slot,
+) -> Result<Slot> {
+    let mut candidate_slot = starting_candidate;
+    let mut off_chain_state_root =
+        states::get_state_root_by_slot(db_pool, candidate_slot).await;
+
+    // take the init slot value query beacon chain to get the given slot's state_root value from beacon chain's response message
+    let mut on_chain_state_root = beacon_node
+        .get_header_by_slot(candidate_slot)
+        .await?
+        .map(|envelope| envelope.header.message.state_root);
+
+    loop {
+        match (off_chain_state_root, on_chain_state_root) {
+            (Some(off_chain_state_root), Some(on_chain_state_root))
+                if off_chain_state_root == on_chain_state_root =>
+            {
+                debug!(off_chain_state_root, on_chain_state_root, "off-chain and on-chain state root value match by given slot: {candidate_slot}");
+                break;
+            }
+
+            _ => {
+                // refresh the candidate_slot minus it by 1
+                candidate_slot = candidate_slot - 1;
+
+                // continue query off chain state_root value via the new candidate_slot  --> local db table beacon-_states
+                off_chain_state_root =
+                    states::get_state_root_by_slot(db_pool, candidate_slot)
+                        .await;
+
+                // continue query on chain state_root value via the new candidate_slot --> parse from beacon endpoint response message
+                on_chain_state_root = beacon_node
+                    .get_header_by_slot(candidate_slot)
+                    .await?
+                    .map(|msg| msg.header.message.state_root);
+            }
+        }
+    } // loop
+
+    debug!(
+        slot = candidate_slot.0,
+        "found a state match between stored and on-chain"
+    );
+    Ok(candidate_slot)
+}
