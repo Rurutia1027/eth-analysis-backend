c372a3fc40f7e954afbd37ce38d1643e89cffee0
Rurutia1027 <rurutia1027@gmail.com>
2025-02-07 19:40:03 +0800

    add beacon tables rollback functions by given slot value

--- src/beacon_chain/syncer/mod.rs
+++ src/beacon_chain/syncer/mod.rs
@@ -429,3 +429,65 @@ async fn stream_slots_from_last(db_pool: &PgPool) -> impl Stream<Item = Slot> {
     // then we got the next slot value to be sync from beacon endpoint is LOCAL_LATEST_SLOT_VALUE + 1
     stream_slots_from(next_slot_to_sync).await
 }
+
+// this function will delete multiple records from beacon tables,
+// that the records locates by the given slot range [given_slot, ...)
+pub async fn rollback_slots(
+    executor: &mut PgConnection,
+    greater_than_or_equal: Slot,
+) -> Result<()> {
+    debug!("rolling back data based on slots locates in range of [{greater_than_or_equal}, ...]");
+    let mut transaction = executor.begin().await?;
+    // todo: update table eth_supply but we haven't implement this table's associated function yet, leave a todo here
+    blocks::delete_blocks(&mut *transaction, greater_than_or_equal).await;
+    issuance::delete_issuances(&mut *transaction, greater_than_or_equal).await;
+    balances::delete_validator_sums(&mut *transaction, greater_than_or_equal)
+        .await;
+    states::delete_states(&mut *transaction, greater_than_or_equal).await;
+    transaction.commit().await?;
+    Ok(())
+}
+
+// this function will delete records from multiple beacon tables
+// that the records in the beacon tables share the same slot value provided by the parameter
+pub async fn rollback_slot(
+    executor: &mut PgConnection,
+    slot: Slot,
+) -> Result<()> {
+    debug!("rolling back data from db tables based on the given slot {slot}");
+    let mut transaction = executor.begin().await?;
+    // todo: update table eth_supply but we haven't implement this table's associated function yet, leave a todo here
+    // first - delete block record in beacon_blocks table that the block locates in the given slot period(12 s) on beacon chain
+    blocks::delete_block(&mut *transaction, slot).await;
+
+    // second - delete issuance records in beacon_issuance table
+    issuance::delete_issuance(&mut *transaction, slot).await;
+
+    // third - delete validator sum from beacon_validators_balance tabel
+    balances::delete_validator_sum(&mut *transaction, slot).await;
+
+    // last -- delete record from table beacon_states -- this should be the last delete, because the above table deletion all refers to
+    // record in beacon_states
+    states::delete_state(&mut *transaction, slot).await;
+    transaction.commit().await?;
+    Ok(())
+}
+
+// calculate the slot lag between on chain slot and local(off chain) slot value
+async fn estimate_slots_remaining(
+    executor: impl PgExecutor<'_>,
+    beacon_node: &BeaconNodeHttp,
+) -> i32 {
+    // on beacon chain latest slot value (slot value is increase and beacon chain global unique value)
+    let last_slot_on_chain = beacon_node.get_last_header().await.unwrap();
+
+    // off chain local recorded latest slot value
+    let last_slot_off_chain = states::get_last_state(executor)
+        .await
+        .map_or(Slot(0), |state| state.slot);
+
+    // calculate how many slots remain to be sync from remote to local
+    let lag = last_slot_on_chain.slot().0 - last_slot_off_chain.0;
+    debug!("#estimate_slots_remaining {}", lag);
+    return lag;
+}
