ee9ac53d6040290f9d624af8584f3d1ee9e8e401
Rurutia1027 <rurutia1027@gmail.com>
2025-02-26 14:37:39 +0800

    update setup pg docker script

--- bin/setup-docker-db.sh
+++ bin/setup-docker-db.sh
@@ -1,3 +1,7 @@
 #!/bin/zsh
 
 docker run --name my-postgres -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -e POSTGRES_DB=defaultdb -p 5432:5432 -d postgres
+
+echo ("load migrations folder scripts to target database 'defaultdb' ");
+
+cd ../ && cargo install sqlx-cli && cargo sqlx migrate run
