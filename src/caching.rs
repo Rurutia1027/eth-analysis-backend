a23c0e69c160035b57384fccb97046a3ee36b506
Rurutia1027 <rurutia1027@gmail.com>
2025-03-01 23:41:10 +0800

    add codes for caching and caching keys

---
+++ src/caching.rs
@@ -0,0 +1,193 @@
+use std::fmt::Display;
+use std::str::FromStr;
+use enum_iterator::Sequence;
+use log::debug;
+use serde::Serialize;
+use serde_json::Value;
+use sqlx::{PgExecutor, PgPool};
+use thiserror::Error;
+use crate::{
+    kv_store::{self, KvStore},
+    time_frames::{GrowingTimeFrame, LimitedTimeFrame, TimeFrame},
+};
+
+
+#[derive(Debug, Clone, Copy, Eq, Hash, PartialEq, Sequence)]
+pub enum CacheKey {
+    AverageEthPrice,
+    BaseFeeOverTime,
+    BaseFeePerGas,
+    BaseFeePerGasBarrier,
+    BaseFeePerGasStats,
+    BaseFeePerGasStatsTimeFrame(TimeFrame),
+    BlobFeePerGasStats,
+    BlobFeePerGasStatsTimeFrame(TimeFrame),
+    BlockLag,
+    BurnRates,
+    BurnSums,
+    EffectiveBalanceSum,
+    EthPrice,
+    FlippeningData,
+    GaugeRates,
+    SupplyParts,
+    IssuanceBreakdown,
+    IssuanceEstimate,
+    SupplyChanges,
+    SupplyDashboardAnalysis,
+    SupplyOverTime,
+    SupplyProjectionInputs,
+    SupplySinceMerge,
+    TotalDifficultyProgress,
+    ValidatorRewards,
+}
+
+impl CacheKey {
+    pub fn to_db_key(self) -> &'static str {
+        use CacheKey::*;
+        use GrowingTimeFrame::*;
+        use LimitedTimeFrame::*;
+        use TimeFrame::*;
+
+        match self {
+            AverageEthPrice => "average-eth-price",
+            BaseFeeOverTime => "base-fee-over-time",
+            BaseFeePerGas => "current-base-fee",
+            BaseFeePerGasBarrier => "base-fee-per-gas-barrier",
+            BaseFeePerGasStats => "base-fee-per-gas-stats",
+            BaseFeePerGasStatsTimeFrame(time_frame) => match time_frame {
+                Growing(SinceBurn) => "base-fee-per-gas-stats-since_burn",
+                Growing(SinceMerge) => "base-fee-per-gas-stats-since_merge",
+                Limited(Minute5) => "base-fee-per-gas-stats-m5",
+                Limited(Hour1) => "base-fee-per-gas-stats-h1",
+                Limited(Day1) => "base-fee-per-gas-stats-d1",
+                Limited(Day7) => "base-fee-per-gas-stats-d7",
+                Limited(Day30) => "base-fee-per-gas-stats-d30",
+            },
+            BlobFeePerGasStats => "blob-fee-per-gas-stats",
+            BlobFeePerGasStatsTimeFrame(time_frame) => match time_frame {
+                Growing(SinceBurn) => "blob-fee-per-gas-stats-since_burn",
+                Growing(SinceMerge) => "blob-fee-per-gas-stats-since_merge",
+                Limited(Minute5) => "blob-fee-per-gas-stats-m5",
+                Limited(Hour1) => "blob-fee-per-gas-stats-h1",
+                Limited(Day1) => "blob-fee-per-gas-stats-d1",
+                Limited(Day7) => "blob-fee-per-gas-stats-d7",
+                Limited(Day30) => "blob-fee-per-gas-stats-d30",
+            },
+            BlockLag => "block-lag",
+            BurnRates => "burn-rates",
+            BurnSums => "burn-sums",
+            EffectiveBalanceSum => "effective-balance-sum",
+            EthPrice => "eth-price",
+            FlippeningData => "flippening-data",
+            GaugeRates => "gauge-rates",
+            IssuanceBreakdown => "issuance-breakdown",
+            IssuanceEstimate => "issuance-estimate",
+            SupplyChanges => "supply-changes",
+            SupplyDashboardAnalysis => "supply-dashboard-analysis",
+            SupplyOverTime => "supply-over-time",
+            SupplyParts => "supply-parts",
+            SupplyProjectionInputs => "supply-projection-inputs",
+            SupplySinceMerge => "supply-since-merge",
+            TotalDifficultyProgress => "total-difficulty-progress",
+            ValidatorRewards => "validator-rewards",
+        }
+    }
+}
+
+impl Display for CacheKey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_db_key())
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum ParseCacheKeyError {
+    #[error("failed to parse cache key {0}")]
+    UnknownCacheKey(String),
+}
+
+
+impl FromStr for CacheKey {
+    type Err = ParseCacheKeyError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "average-eth-price" => Ok(Self::AverageEthPrice),
+            "base-fee-over-time" => Ok(Self::BaseFeeOverTime),
+            "current-base-fee" => Ok(Self::BaseFeePerGas),
+            "base-fee-per-gas" => Ok(Self::BaseFeePerGas),
+            "base-fee-per-gas-barrier" => Ok(Self::BaseFeePerGasBarrier),
+            "base-fee-per-gas-stats" => Ok(Self::BaseFeePerGasStats),
+            "blob-fee-per-gas-stats" => Ok(Self::BlobFeePerGasStats),
+            "block-lag" => Ok(Self::BlockLag),
+            "burn-rates" => Ok(Self::BurnRates),
+            "burn-sums" => Ok(Self::BurnSums),
+            "effective-balance-sum" => Ok(Self::EffectiveBalanceSum),
+            "eth-price" => Ok(Self::EthPrice),
+            "flippening-data" => Ok(Self::FlippeningData),
+            "gauge-rates" => Ok(Self::GaugeRates),
+            "issuance-breakdown" => Ok(Self::IssuanceBreakdown),
+            "issuance-estimate" => Ok(Self::IssuanceEstimate),
+            "supply-changes" => Ok(Self::SupplyChanges),
+            "supply-dashboard-analysis" => Ok(Self::SupplyDashboardAnalysis),
+            "supply-over-time" => Ok(Self::SupplyOverTime),
+            "supply-parts" => Ok(Self::SupplyParts),
+            "supply-projection-inputs" => Ok(Self::SupplyProjectionInputs),
+            "supply-since-merge" => Ok(Self::SupplySinceMerge),
+            "total-difficulty-progress" => Ok(Self::TotalDifficultyProgress),
+            "validator-rewards" => Ok(Self::ValidatorRewards),
+            unknown_key if unknown_key.starts_with("base-fee-per-gas-stats-") => unknown_key
+                .split('-')
+                .nth(5)
+                .expect(
+                    "expect keys which start with 'base-fee-per-gas-stats-' to have a time frame",
+                )
+                .to_string()
+                .parse::<TimeFrame>()
+                .map_or(
+                    Err(ParseCacheKeyError::UnknownCacheKey(unknown_key.to_string())),
+                    |key| Ok(Self::BaseFeePerGasStatsTimeFrame(key)),
+                ),
+            unknown_key => Err(ParseCacheKeyError::UnknownCacheKey(unknown_key.to_string())),
+        }
+    }
+}
+
+
+pub async fn publish_cache_update<'a>(executor: impl PgExecutor<'a>, key: &CacheKey) {
+    sqlx::query!(
+        "
+            SELECT pg_notify('cache-update', $1)
+        ",
+        key.to_db_key()
+    )
+        .execute(executor)
+        .await
+        .unwrap();
+}
+
+pub async fn get_serialized_caching_value(
+    key_value_store: &impl KvStore,
+    cache_key: &CacheKey,
+) -> Option<Value> {
+    key_value_store.get(cache_key.to_db_key()).await
+}
+
+pub async fn set_value<'a>(
+    executor: impl PgExecutor<'_>,
+    cache_key: &CacheKey,
+    value: impl Serialize,
+) {
+    kv_store::set_value(
+        executor,
+        cache_key.to_db_key(),
+        &serde_json::to_value(value).expect("expect value to be serializable"),
+    )
+        .await;
+}
+
+pub async fn update_and_publish(db_pool: &PgPool, cache_key: &CacheKey, value: impl Serialize) {
+    set_value(db_pool, cache_key, value).await;
+    publish_cache_update(db_pool, cache_key).await;
+}
+
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,1 +1,43 @@
+use std::sync::Arc;
+use axum::Extension;
+use chrono::{DateTime, Duration, Utc};
+use lazy_static::lazy_static;
+use log::info;
+use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
+use crate::db::db;
+use crate::kv_store::KVStorePostgres;
+use crate::server::caching::Cache;
 
+mod caching;
+
+
+lazy_static!{
+    static ref FOUR_SECONDS: Duration = Duration::seconds(4);
+    static ref ONE_DAY: Duration = Duration::days(1);
+    static ref ONE_MINUTE: Duration = Duration::minutes(1);
+}
+
+pub struct State {
+    pub cache: Cache,
+    pub db_pool: PgPool,
+
+}
+
+pub type StateExtension = Extension<Arc<State>>;
+
+
+pub async fn start_server() {
+    info!("starting serve fees");
+    let started_on : DateTime<Utc> = chrono::Utc::now();
+    let db_pool =db::get_db_pool("eth-analysis-server", 3).await;
+    let kv_store: KVStorePostgres = KVStorePostgres::new(db_pool.clone());
+
+    // todo: init Cache
+    info!("cache ready");
+
+    // todo: init health
+    info!("health ready");
+
+
+}
