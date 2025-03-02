64f4ec7e415f89a1de273c5919e1d5a1e8c6f2ac
Rurutia1027 <rurutia1027@gmail.com>
2025-03-02 23:10:20 +0800

    add codes for relay api

--- src/mev_blocks/relay_api.rs
+++ src/mev_blocks/relay_api.rs
@@ -1,6 +1,7 @@
 use super::MevBlock;
 use crate::units::WeiNewtype;
 use async_trait::async_trait;
+use http_body_util::BodyExt;
 use mockall::{automock, predicate::*};
 use serde::Deserialize;
 
@@ -34,3 +35,66 @@ impl TryFrom<MaybeMevBlock> for MevBlock {
         }
     }
 }
+
+#[automock]
+#[async_trait]
+pub trait RelayApi {
+    async fn fetch_mev_blocks(
+        &self,
+        start_slot: i32,
+        end_slot: i32,
+    ) -> Vec<MevBlock>;
+}
+
+pub struct RelayApiHttp {
+    server_url: String,
+    client: reqwest::Client,
+}
+
+impl RelayApiHttp {
+    pub fn new() -> Self {
+        RelayApiHttp {
+            server_url: "https://relay.ultrasound.money".into(),
+            client: reqwest::Client::new(),
+        }
+    }
+
+    pub fn new_with_url(server_url: &str) -> Self {
+        Self {
+            server_url: server_url.into(),
+            client: reqwest::Client::new(),
+        }
+    }
+}
+
+impl Default for RelayApiHttp {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[async_trait]
+impl RelayApi for RelayApiHttp {
+    async fn fetch_mev_blocks(
+        &self,
+        start_slot: i32,
+        end_slot: i32,
+    ) -> Vec<MevBlock> {
+        self.client
+            .get(format!(
+                "{}/api/block-production?start_slot={}&end_slot={}",
+                self.server_url, start_slot, end_slot
+            ))
+            .send()
+            .await
+            .unwrap()
+            .json::<Vec<MaybeMevBlock>>()
+            .await
+            .unwrap()
+            .into_iter()
+            .filter_map(|item| item.try_into().ok())
+            .collect()
+    }
+}
+
+
